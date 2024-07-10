1a14e2ca9fa98e1cf7adaa7c943bbd58168009e1
Áron Németh <anemeth@dmhlab.hu>
2024-07-10 15:58:27 +0200

    Responsive embeded youtube player. Reorganise file

--- mafima.py
+++ mafima.py
@@ -10,20 +10,6 @@ data: pd.DataFrame = pd.read_csv("mafima.csv", delimiter=";")
 
 
 def main():
-    st.markdown(
-        """
-        <div style="text-align: center;">
-            <h2>Magyar Film Maraton Toplista</h2>
-        </div>""",
-        unsafe_allow_html=True,
-    )
-    st.markdown(
-        """
-        <div style="text-align: center;">
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?si=hY9MZAi0jL3n4Je_&amp;list=PLAuHYXdwFUwg8ErChl6ZtdloiZg4xI55K" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        </div>""",
-        unsafe_allow_html=True,
-    )
     with st.sidebar:
         st.subheader("Szűrők")
         year_filter = st.slider(
@@ -64,6 +50,29 @@ def main():
         )
 
     filtered_data = filtered_data.sort_values("Értékelés", ascending=False)
+
+    st.markdown(
+        """
+        <div style="text-align: center;">
+            <h2>Magyar Film Maraton Toplista</h2>
+        </div>""",
+        unsafe_allow_html=True,
+    )
+    st.markdown(
+        """
+    <div style="position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; background: #000;">
+        <iframe style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; border: 0;"
+                src="https://www.youtube.com/embed/videoseries?si=hY9MZAi0jL3n4Je_&amp;list=PLAuHYXdwFUwg8ErChl6ZtdloiZg4xI55K"
+                title="Magyar Film Maraton"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerpolicy="strict-origin-when-cross-origin"
+                allowfullscreen>
+        </iframe>
+    </div>
+    """,
+        unsafe_allow_html=True,
+    )
+    st.divider()
 
     # Configure AgGrid options
     gb = GridOptionsBuilder.from_dataframe(filtered_data)
